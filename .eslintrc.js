9ca53c3d327a86454e49fec83779c1eac98065f5
Anderson Kennedy <andersonkennedydev@gmail.com>
2020-02-17 17:13:45 +0100

    configuração da styleGuide

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
